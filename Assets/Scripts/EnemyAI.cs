8443c8a6cff852badc6940eda6a9ae4e7ab96756
Brice Yuuki Gibson <Kakipea888@gmail.com>
2023-03-11 19:42:42 -0600

    Enemy Attacks Player
    Enemy Ai can now attack the player once close enough and continues to chase the player once provoked.

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -6,17 +6,56 @@ using UnityEngine.AI;
 public class EnemyAI : MonoBehaviour
 {
     [SerializeField] Transform target;
-    NavMeshAgent navMeshAgent;
+    [SerializeField] float chaseRange = 10f;
 
-    // Start is called before the first frame update
+    NavMeshAgent navMeshAgent;
+    float distanceToTarget = Mathf.Infinity;
+    bool isProvoked = false;
+
     void Start()
     {
         navMeshAgent = GetComponent<NavMeshAgent>();
     }
 
-    // Update is called once per frame
     void Update()
     {
+        distanceToTarget = Vector3.Distance(target.position, transform.position);
+        if (isProvoked) 
+        {
+            EngageTarget();
+        }
+        else if (distanceToTarget <= chaseRange)
+        {
+            isProvoked = true;
+        }
+    }
+
+    private void EngageTarget()
+    {
+        if (distanceToTarget >=navMeshAgent.stoppingDistance)
+        {
+            ChaseTarget();
+        }
+
+        if (distanceToTarget <= navMeshAgent.stoppingDistance)
+        {
+            AttackTarget();
+        }
+    }
+
+    private void ChaseTarget()
+    {
         navMeshAgent.SetDestination(target.position);
+    }
+
+    private void AttackTarget()
+    {
+        Debug.Log(name + "has seeked and is destroying" + target.name);
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, chaseRange);
     }
 }
