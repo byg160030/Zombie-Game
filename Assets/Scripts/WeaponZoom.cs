116ddf19266ab8455e1eacf0ddcf9d943e90b4ca
Brice Yuuki Gibson <Kakipea888@gmail.com>
2023-06-12 21:10:11 -0500

    New Stage Added and Various other Updates
    A new stage is added as well as Ammo Pickup, flash light, batteries for flash lights.

--- Assets/Scripts/WeaponZoom.cs
+++ Assets/Scripts/WeaponZoom.cs
@@ -13,24 +13,38 @@ public class WeaponZoom : MonoBehaviour
 
     bool zoomedInToggle = false;
 
+    private void OnDisable()
+    {
+        ZoomOut();
+    }
+
     private void Update()
     {
         if(Input.GetMouseButtonDown(1))
         {
             if(zoomedInToggle == false)
             {
-                zoomedInToggle = true;
-                fpsCamera.fieldOfView = zoomedInFOV;
-                fpsController.lookSpeedX = zoomInSensitivity;
-                fpsController.lookSpeedY = zoomInSensitivity;
+                ZoomIn();
             }
             else
             {
-                zoomedInToggle = false;
-                fpsCamera.fieldOfView = zoomedOutFOV;
-                fpsController.lookSpeedX = zoomOutSensitivity;
-                fpsController.lookSpeedY = zoomOutSensitivity;
+                ZoomOut();
             }
         }
+    }
+    private void ZoomIn()
+    {
+        zoomedInToggle = true;
+        fpsCamera.fieldOfView = zoomedInFOV;
+        fpsController.lookSpeedX = zoomInSensitivity;
+        fpsController.lookSpeedY = zoomInSensitivity;
+    }
+
+    private void ZoomOut()
+    {
+        zoomedInToggle = false;
+        fpsCamera.fieldOfView = zoomedOutFOV;
+        fpsController.lookSpeedX = zoomOutSensitivity;
+        fpsController.lookSpeedY = zoomOutSensitivity;
     }
 }
